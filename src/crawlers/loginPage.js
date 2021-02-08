c2f5bdd765972a866f1e8d0179126f75b26d76dc
moki298 <moki298@gmail.com>
2021-02-08 17:34:28 -0500

    detailed comments for login page

--- src/crawlers/loginPage.js
+++ src/crawlers/loginPage.js
@@ -24,13 +24,20 @@ class LoginPage {
             // click Sign In
             await page.click(selectors.loginPage.signInButton)
 
-            // check if the user has two factor authentication enabled
-            // I am verifying this by checking for UI elements in the page that appears once the user clicks the sign-in button
+            // check if the user has 2FA enabled
+            // as of now robinhood supports 2FA for sms and authenticator app
 
-            // the below two promises are in a race condition, which ever resolves first executes the `then` part
-            // the other promise will eventually fail due to timeout error which is being caught
+            // if 2FA is enabled the user is requested to enter a six digit code to get login access
+            // even if 2FA is disabled the user will still be asked to enter a six digit code 
+            // since he is logging-in for the first time in the browser
+
+            // below i am using `waitForFunction` and `waitForSelector` functions to check if 2FA is enabled
+
+            // the two promises are in a race condition, which ever resolves first executes the `then` part
+            // the other promise will eventually fail due to a timeout error which is being caught
 
             // promise one
+            // checking for `sms` button for accounts with with 2FA disabled
             page.waitForFunction(smsConfirmationButton => {
                 if (document.querySelector(smsConfirmationButton).innerText === "SMS") {
                     return true
@@ -57,6 +64,7 @@ class LoginPage {
             }).catch(err => { })
 
             // promise two
+            // checking for button with name `mfa_code` for accounts with 2FA enabled
             page.waitForSelector(selectors.loginPage.mfaFieldForTwoFactAuth, { timeout: 5000 }).then(async () => {
                 await page.waitForFunction(mfaFieldForTwoFactAuth => {
                     let keyLength = document.querySelector(mfaFieldForTwoFactAuth).value.length
